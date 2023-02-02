7eb907f3a6b89166cb38909113e89e10c30e03e4
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2023-02-02 11:23:04 +0100

    set up files

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="public/css/main.css">
+    <script src='https://unpkg.com/vue@3'></script>
+    <script src='https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js'></script>
+</head>
+
+<body>
+    <div class="content">
+        <header class="header">
+
+        </header>
+
+        <main class="main">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Deserunt neque et dolor fuga doloremque, totam doloribus qui dolore ad reiciendis
+                provident magni dignissimos odit, quasi impedit consequuntur iusto ut. Maxime?
+            </p>
+        </main>
+    </div>
+</body>
+
+</html>
