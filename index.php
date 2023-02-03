57725056f9b6b498eed766ba0eecb3749e5eb008
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2023-02-03 18:37:35 +0100

    set up files

--- index.php
+++ index.php
@@ -8,73 +8,262 @@
 </head>
 
 <body>
+    <?php include 'parts/header.php'; ?>
+
     <div class="content">
-        <div class="ticket">
-            <header class="header">
-                <img src="public/img/food.png" alt="" class='food'>
-                <img src="public/img/bandeau.png" alt="" class='flag'>
-                <img src="public/img/food.png" alt="" class='food'>
-            </header>
+        <div class="hero w-100 text-center">
+            <h1>
+                Mister POke
+            </h1>
 
-            <main class="main">
-                <p class="top-text">
-                    Concours du <span>Mardi 14 Janvier 2023</span>
-                </p>
+            <p class="hero__text">
+                Appréciez la Cuisine Hawaïenne
+            </p>
 
-                <p class="text">
-                    Pour valider votre participation au <strong>concours Mister Poke</strong>,
-                    scannez le <strong>code QR</strong> et suivez la page facebook <strong>@Mrpoke</strong>
-                </p>
+            <a class="btn btn-primary" href="">
+                Commander
+            </a>
+        </div>
 
-                <div class="row">
-                    <img src="public/img/phone.png" alt="">
-                    <div class="box">
-                        2 menus Maxi poke à gagner
-                    </div>
-                </div>
-
-                <div class="infos">
-                    <div class="info">
-                        Info: <div class="blue">0696109953</div>
-                    </div>
-
-                    <div class="info">
-                        Info: <div class="blue">0696610888</div>
-                    </div>
-                </div>
-
-                <div class="bottom">
-                    <div class="details">
-                        <img src="public/img/qr_code_63da66143f8be.png" class='qr' alt="">
-
-                        <div class="details__main">
-                            <p>
-                                Votre numéro de participation:
-
-                            </p>
-
-                            <div class="strong">
-                                --- 519645 ---
-                            </div>
-
-                            <div class="thin">
-                                ( A présenter en cas de gain)
-                            </div>
+        <div class="container mt-2">
+            <div class="row services">
+                <div class="col-md-6 bg-light col-sm-12 icons mt-2">
+                    <div class="service">
+                        <div class="service__img">
+                            <img src="public/img/icon-service1.jpg" alt="">
                         </div>
 
-                        <img src="public/img/bowl.png" alt="">
+                        <div class="service__text">
+                            <span>
+                                100% healthy
+                            </span> <br>
+
+                            <p class="text text-grey">
+                                Poisson et légumes frais
+                            </p>
+                        </div>
+
+
                     </div>
 
-                    <div class="black">
-                        <p>
-                            Avec ce coupon vous bénéficiez de 10% de réduction
-                            72h après la date du concours
-                        </p>
+                    <div class="service">
+                        <div class="service__img">
+                            <img src="public/img/icon-service1.jpg" alt="">
+                        </div>
+
+                        <div class="service__text">
+                            <span>
+                                Cuisine saine
+                            </span> <br>
+
+                            <p class="text text-grey">
+                                Poisson et légumes frais
+                            </p>
+                        </div>
+
+                    </div>
+
+                    <div class="service">
+                        <div class="service__img">
+                            <img src="public/img/icon-service3.jpg" alt="">
+                        </div>
+
+                        <div class="service__text">
+                            <span>
+                                Livraison offerte
+                            </span> <br>
+
+                            <p class="text text-grey">
+                                A partir de 12 eur
+                            </p>
+                        </div>
+
                     </div>
                 </div>
-            </main>
+
+                <div class="col-md-6 col-sm-12 image">
+                    <img src="public/img/services1.jpg" alt="">
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row gallery text-center">
+                <h2 class="">
+                    Appréciez la Cuisine Hawaïenne
+                </h2>
+
+                <p class="text">
+                    Sur place, à emporter ou en réservation
+                </p>
+            </div>
+
+            <div class="row">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="row">
+                                <div class="col-4">
+                                    <img class="d-block w-100" src="public/img/food12.jpg"
+                                        alt="specialite hawaienne en martinique">
+                                </div>
+
+                                <div class="col-4">
+                                    <img class="d-block w-100" src="public/img/food3.jpg"
+                                        alt="specialite hawaienne en martinique">
+                                </div>
+
+                                <div class="col-4">
+                                    <img class="d-block w-100" src="public/img/food4.jpg"
+                                        alt="specialite hawaienne en martinique">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="row">
+                                <div class="col-4">
+                                    <img class="d-block w-100" src="public/img/food4.jpg"
+                                        alt="specialite hawaienne en martinique">
+                                </div>
+
+                                <div class="col-4">
+                                    <img class="d-block w-100" src="public/img/food5.jpg"
+                                        alt="specialite hawaienne en martinique">
+                                </div>
+
+                                <div class="col-4">
+                                    <img class="d-block w-100" src="public/img/food6.jpg"
+                                        alt="specialite hawaienne en martinique">
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="carousel-item">
+                            <div class="row">
+                                <div class="col-4">
+                                    <img class="d-block w-100" src="public/img/food7.jpg"
+                                        alt="specialite hawaienne en martinique">
+                                </div>
+
+                                <div class="col-4">
+                                    <img class="d-block w-100" src="public/img/food8.jpg"
+                                        alt="specialite hawaienne en martinique">
+                                </div>
+
+                                <div class="col-4">
+                                    <img class="d-block w-100" src="public/img/food9.jpg"
+                                        alt="specialite hawaienne en martinique">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Précédent</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Suivant</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="container mt-5">
+            <div class="row about">
+                <div class="col-sm-12 col-md-6">
+                    <div class="container">
+                        <div class="row">
+                            <img src="public/img/hawai1.jpg" alt="" class="col-6">
+                            <img src="public/img/hawai2.jpg" alt="" class="col-6">
+                            <img src="public/img/hawai3.jpg" alt="" class="col-6">
+                            <img src="public/img/hawai4.jpg" alt="" class="col-6">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-12 col-md-6">
+                    <h2>
+                        Mister Poke 972
+                    </h2>
+
+                    <p class="text">
+                        Mrpoke972 est un restaurant spécialisé dans la vente du Poke,
+                        un plat hawaïen traditionnel. Ce restaurant propose des plats
+                        frais et savoureux, préparés avec des ingrédients de qualité
+                        supérieure pour garantir une expérience culinaire inoubliable.
+                        Le Poke est un mélange de poisson cru, de riz et de légumes,
+                        le tout assaisonné avec des sauces délicieuses. Outre le Poke,
+                        le menu de Mrpoke972 propose également une variété de salades, de fruits de mer et de soupes,
+                        chacun préparé avec soin et passion.
+
+                        En visitant Mrpoke972, vous découvrirez non seulement les saveurs authentiques d'Hawaii, mais
+                        également un environnement accueillant et chaleureux. Le personnel amical et attentionné se
+                        tient à votre disposition pour vous aider à faire le meilleur choix parmi les différents plats
+                        proposés. Que vous soyez à la recherche d'un délicieux déjeuner ou d'un dîner en soirée,
+                        Mrpoke972 est l'endroit idéal pour déguster un repas sain et nourrissant. Alors n'hésitez plus
+                        et venez découvrir les saveurs hawaïennes chez Mrpoke972!
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row ads">
+                <div class="col-sm-12 col-md-3 box">
+                    <img src="public/img/ad4.jpg" alt="repas au poisson en martinique">
+                </div>
+                <div class="col-sm-12 col-md-3 box">
+                    <img src="public/img/ad3.jpg" alt="repas au poisson en martinique">
+                </div>
+                <div class="col-sm-12 col-md-3 box">
+                    <img src="public/img/ad1.jpg" alt="repas au poisson en martinique">
+                </div>
+
+                <div class="col-sm-12 col-md-3 box">
+                    <img src="public/img/ad5.jpg" alt="repas au poisson en martinique">
+                </div>
+            </div>
+        </div>
+
+        <div class="game text-center">
+            <p class="text text-black">
+                Avez vous entendu parler de notre jeu concours ??
+            </p>
+
+            <div class="audio">
+                <audio src=""></audio>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row book">
+                <div class="col-sm-12 col-md-4">
+                    <div class="book__form">
+                        <p class="text">
+                            Cliquez ici pour réserver votre repas maintenant
+                        </p>
+                        <button class="btn btn-success">
+                            Commander
+                        </button>
+                    </div>
+                </div>
+
+                <div class="col-sm-12 col-md-8">
+                    <div class="map mx-auto">
+
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+
+    <?php include 'parts/footer.php'; ?>
 </body>
 
 </html>
---
+++ parts/footer.php
@@ -0,0 +1,50 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-4">
+                <h3>
+                    Adresse
+                </h3> <br>
+
+                <p class="text text-left">
+                    <i class="bi bi-geo-alt"></i>
+                    53-55 Rue Moreau De Jones 97200 Le Fort-de-France,
+                    Martinique
+                </p>
+            </div>
+
+            <div class="col-sm-12 col-md-4">
+                <h3>
+                    Horaires
+                </h3> <br>
+
+                <p class="text text-left">
+                    🛍 A emporter de 10h30-14h30•18h30-21h30 <br>
+                    🛵 Livraison 10h30-14h00•18h30-21h15
+                </p>
+            </div>
+
+            <div class="col-sm-12 col-md-4">
+                <h3>
+                    Contact
+                </h3> <br>
+
+                <p class="text text-left">
+                    <i class="bi bi-telephone"></i> +596 696 10 99 35
+                    <i class="bi bi-envelope"></i> contact@misterpoke972.com
+
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+</script>
